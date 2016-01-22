6fec5d12df1a18b3f89ac13c655fb28ea4501584
Evan Teran <evan.teran@gmail.com>
2016-01-21 21:13:07 -0500

    backticks

--- source/regularExp.h
+++ source/regularExp.h
@@ -37,7 +37,7 @@
 #define NSUBEXP 50u
 
 /* Structure to contain the compiled form of a regular expression plus
-   pointers to matched text.  `program' is the actual compiled regex code. */
+   pointers to matched text.  'program' is the actual compiled regex code. */
 
 class regexp {
 public:
@@ -63,23 +63,23 @@ enum RE_DEFAULT_FLAG {
 	/* REDFLT_MATCH_NEWLINE = 2    Currently not used. */
 };
 
-/* Match a `regexp' structure against a string. */
+/* Match a 'regexp' structure against a string. */
 
 int ExecRE(regexp *prog,               /* Compiled regex. */
            const char *string,         /* Text to search within. */
-           const char *end,            /* Pointer to the end of `string'.  If NULL will scan from `string' until '\0' is found. */
+           const char *end,            /* Pointer to the end of 'string'.  If NULL will scan from 'string' until '\0' is found. */
            bool reverse,               /* Backward search. */
-           char prev_char,             /* Character immediately prior to `string'.  Set to '\n' or '\0' if true beginning of text. */
-           char succ_char,             /* Character immediately after `end'.  Set to '\n' or '\0' if true beginning of text. */
+           char prev_char,             /* Character immediately prior to 'string'.  Set to '\n' or '\0' if true beginning of text. */
+           char succ_char,             /* Character immediately after 'end'.  Set to '\n' or '\0' if true beginning of text. */
            const char *delimiters,     /* Word delimiters to use (NULL for default) */
            const char *look_behind_to, /* Boundary for look-behind; defaults to "string" if NULL */
            const char *match_till);    /* Boundary to where match can extend. \0 is assumed to be the boundary if not set. Lookahead can cross the boundary. */
 
-/* Perform substitutions after a `regexp' match. */
+/* Perform substitutions after a 'regexp' match. */
 bool SubstituteRE(const regexp *prog, const char *source, char *dest, const int max);
 
-/* Builds a default delimiter table that persists across `ExecRE' calls that
-   is identical to `delimiters'.  Pass NULL for "default default" set of
+/* Builds a default delimiter table that persists across 'ExecRE' calls that
+   is identical to 'delimiters'.  Pass NULL for "default default" set of
    delimiters. */
 
 void SetREDefaultWordDelimiters(view::string_view delimiters);
