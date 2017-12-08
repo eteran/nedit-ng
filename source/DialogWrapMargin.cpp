2c3f8fe2b39cd971636971617a14d7ccbf579031
Evan Teran <evan.teran@gmail.com>
2017-12-07 23:57:41 -0500

    no need for explicit error checking here, the UI has validation already which prevents bad input

--- source/DialogWrapMargin.cpp
+++ source/DialogWrapMargin.cpp
@@ -16,21 +16,13 @@ void DialogWrapMargin::on_checkWrapAndFill_toggled(bool checked) {
 
 void DialogWrapMargin::on_buttonBox_accepted() {
 
-	int margin;
+    int margin;
 
-	// get the values that the user entered and make sure they're ok 
-	bool wrapAtWindow = ui.checkWrapAndFill->isChecked();
-	if (wrapAtWindow) {
-		margin = 0;
-	} else {
-		
-		margin = ui.spinWrapAndFill->value();
-
-		if (margin <= 0 || margin >= 1000) {
-			QMessageBox::warning(this, tr("Wrap Margin"), tr("Wrap margin out of range"));
-			return;
-		}
-	}
+    if (ui.checkWrapAndFill->isChecked()) {
+        margin = 0;
+    } else {
+        margin = ui.spinWrapAndFill->value();
+    }
 
 	// Set the value in either the requested window or default preferences 
     if(!document_) {
