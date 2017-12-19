7a4301ff292004ec2933a1323b25104274f32b5f
Evan Teran <evan.teran@gmail.com>
2017-12-19 13:48:27 -0500

    enable High DPI icons! no more blurry icons on 4K displays :-)

--- source/nedit.cpp
+++ source/nedit.cpp
@@ -140,6 +140,9 @@ int main(int argc, char *argv[]) {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+
 	QApplication app(argc, argv);
 	QApplication::setWindowIcon(QIcon(QLatin1String(":/res/nedit.png")));
 
