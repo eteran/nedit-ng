d29feb4d6406b2a112056ac4612b2c12c104b958
Evan Teran <evan.teran@gmail.com>
2018-01-08 00:08:54 -0500

    support translation better in Help API

--- source/Help.cpp
+++ source/Help.cpp
@@ -2,19 +2,16 @@
 #include "Help.h"
 #include <QMessageBox>
 
-namespace Help {
-
 /**
  * @brief displayTopic
  * @param topic
  */
-void displayTopic(Help::Topic topic) {
+void Help::displayTopic(Help::Topic topic) {
     Q_UNUSED(topic);
 
     QMessageBox::warning(
                 nullptr,
-                QLatin1String("Not Implemented"),
-                QLatin1String("Sorry, but the help system is not yet implemented!"));
+                tr("Not Implemented"),
+                tr("Sorry, but the help system is not yet implemented!"));
 }
 
-}
--- source/Help.h
+++ source/Help.h
@@ -2,55 +2,58 @@
 #ifndef HELP_H_
 #define HELP_H_
 
-namespace Help {
+#include <QCoreApplication>
 
-enum class Topic {
-    HELP_START,
-    HELP_SELECT,
-    HELP_SEARCH,
-    HELP_CLIPBOARD,
-    HELP_MOUSE,
-    HELP_KEYBOARD,
-    HELP_FILL,
-    HELP_INTERFACE,
-    HELP_FORMAT,
-    HELP_PROGRAMMER,
-    HELP_TABS,
-    HELP_INDENT,
-    HELP_SYNTAX,
-    HELP_TAGS,
-    HELP_CALLTIPS,
-    HELP_BASICSYNTAX,
-    HELP_ESCAPESEQUENCES,
-    HELP_PARENCONSTRUCTS,
-    HELP_ADVANCEDTOPICS,
-    HELP_EXAMPLES,
-    HELP_SHELL,
-    HELP_LEARN,
-    HELP_MACRO_LANG,
-    HELP_MACRO_SUBRS,
-    HELP_RANGESET,
-    HELP_HILITEINFO,
-    HELP_ACTIONS,
-    HELP_CUSTOMIZE,
-    HELP_PREFERENCES,
-    HELP_RESOURCES,
-    HELP_BINDING,
-    HELP_PATTERNS,
-    HELP_SMART_INDENT,
-    HELP_COMMAND_LINE,
-    HELP_SERVER,
-    HELP_RECOVERY,
-    HELP_VERSION,
-    HELP_DISTRIBUTION,
-    HELP_MAILING_LIST,
-    HELP_DEFECTS,
-    HELP_TABS_DIALOG,
-    HELP_CUSTOM_TITLE_DIALOG,
+class Help {
+    Q_DECLARE_TR_FUNCTIONS(Help)
+public:
+    enum class Topic {
+        HELP_START,
+        HELP_SELECT,
+        HELP_SEARCH,
+        HELP_CLIPBOARD,
+        HELP_MOUSE,
+        HELP_KEYBOARD,
+        HELP_FILL,
+        HELP_INTERFACE,
+        HELP_FORMAT,
+        HELP_PROGRAMMER,
+        HELP_TABS,
+        HELP_INDENT,
+        HELP_SYNTAX,
+        HELP_TAGS,
+        HELP_CALLTIPS,
+        HELP_BASICSYNTAX,
+        HELP_ESCAPESEQUENCES,
+        HELP_PARENCONSTRUCTS,
+        HELP_ADVANCEDTOPICS,
+        HELP_EXAMPLES,
+        HELP_SHELL,
+        HELP_LEARN,
+        HELP_MACRO_LANG,
+        HELP_MACRO_SUBRS,
+        HELP_RANGESET,
+        HELP_HILITEINFO,
+        HELP_ACTIONS,
+        HELP_CUSTOMIZE,
+        HELP_PREFERENCES,
+        HELP_RESOURCES,
+        HELP_BINDING,
+        HELP_PATTERNS,
+        HELP_SMART_INDENT,
+        HELP_COMMAND_LINE,
+        HELP_SERVER,
+        HELP_RECOVERY,
+        HELP_VERSION,
+        HELP_DISTRIBUTION,
+        HELP_MAILING_LIST,
+        HELP_DEFECTS,
+        HELP_TABS_DIALOG,
+        HELP_CUSTOM_TITLE_DIALOG,
+    };
+
+    static void displayTopic(Topic topic);
+
 };
-
-void displayTopic(Topic topic);
-
-}
 
 #endif
