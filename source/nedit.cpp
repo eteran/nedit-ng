67a1ff589af04ae6addb36cf85cc9b408ca40891
Evan Teran <evan.teran@gmail.com>
2018-02-15 00:21:08 -0500

    no icons in menus on OSX, they don't show up and mess with alignment. AND OSX style is generally to not have icons in menus anyway.

--- source/nedit.cpp
+++ source/nedit.cpp
@@ -142,6 +142,9 @@ int main(int argc, char *argv[]) {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+#ifdef Q_OS_MACOS
+    QCoreApplication::setAttribute(Qt::AA_DontShowIconsInMenus);
+#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
 
