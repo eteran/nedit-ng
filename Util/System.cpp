471c67c64529cbe5047c31da3cee4211db0535ce
Evan Teran <evan.teran@gmail.com>
2018-08-06 14:38:40 -0400

    minor tweaks to update compiler detection slightly

--- Util/System.cpp
+++ Util/System.cpp
@@ -117,7 +117,7 @@ QLatin1String buildArchitecture() {
 /**
  * @brief buildCompiler
  * @return
- * @note copied from QtCreator ICore
+ * @note adapted from QtCreator src/plugins/coreplugin/icore.cpp: compilerString()
  */
 QString buildCompiler() {
 #if defined(Q_CC_CLANG) // must be before GNU, because clang claims to be GNU too
@@ -125,14 +125,17 @@ QString buildCompiler() {
 #if defined(__apple_build_version__) // Apple clang has other version numbers
 	isAppleString = QLatin1String(" (Apple)");
 #endif
-	return QLatin1String("Clang " ) + QString::number(__clang_major__) + QLatin1Char('.') + QString::number(__clang_minor__) + isAppleString;
+	return QLatin1String("Clang " ) +
+	        QString::number(__clang_major__) +
+	        QLatin1Char('.') +
+	        QString::number(__clang_minor__) +
+	        isAppleString;
 #elif defined(Q_CC_GNU)
-	return QLatin1String("GCC " __VERSION__);
+	return QLatin1String("GCC " ) + QLatin1String(__VERSION__);
 #elif defined(Q_CC_MSVC)
-	if (_MSC_VER >= 1500) { // 1500: MSVC 2008, 1600: MSVC 2010, ...
-		return QLatin1String("MSVC ") + QString::number(2008 + 2 * ((_MSC_VER / 100) - 15));
-	}
-#else
-	return QLatin1String("<Unknown Compiler>");
+	if (_MSC_VER > 1999)  return QLatin1String("MSVC <unknown>");
+	if (_MSC_VER >= 1910) return QLatin1String("MSVC 2017");
+	if (_MSC_VER >= 1900) return QLatin1String("MSVC 2015");
 #endif
+	return QLatin1String("<unknown compiler>");
 }
