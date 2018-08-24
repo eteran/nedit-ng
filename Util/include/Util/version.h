37a149557ba423d44dbf237ebc73f94e5f04bef7
Evan Teran <evan.teran@gmail.com>
2018-08-23 22:54:16 -0400

    using constexpr instead of macros

--- Util/include/Util/version.h
+++ Util/include/Util/version.h
@@ -2,8 +2,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define NEDIT_VERSION_MAJ 2018
-#define NEDIT_VERSION_REV 7
-#define NEDIT_VERSION     (NEDIT_VERSION_MAJ * 1000 + NEDIT_VERSION_REV)
+constexpr auto NEDIT_VERSION_MAJ = 2018;
+constexpr auto NEDIT_VERSION_REV = 7;
+constexpr auto NEDIT_VERSION     = (NEDIT_VERSION_MAJ * 1000 + NEDIT_VERSION_REV);
 
 #endif
