05adcb46dcfb0789982e1322c6993b0efc52c514
Evan Teran <evan.teran@gmail.com>
2020-02-05 00:33:20 -0500

    bumping version in preperation for release :-)

--- Util/include/Util/version.h
+++ Util/include/Util/version.h
@@ -2,8 +2,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-constexpr auto NEDIT_VERSION_MAJ = 2019;
-constexpr auto NEDIT_VERSION_REV = 4;
+constexpr auto NEDIT_VERSION_MAJ = 2020;
+constexpr auto NEDIT_VERSION_REV = 1;
 constexpr auto NEDIT_VERSION     = (NEDIT_VERSION_MAJ * 1000 + NEDIT_VERSION_REV);
 
 #endif
