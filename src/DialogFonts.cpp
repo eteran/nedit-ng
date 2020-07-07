1ccd4feb5c1d6f8c955ff643e743030f0ee22700
TK Soh <teekaysoh@gmail.com>
2020-07-07 12:21:26 +0800

    add new options to Text Fonts dialog on applying new fonts
    1. apply to all open windows/documents
    2. set as default text fonts

--- src/DialogFonts.cpp
+++ src/DialogFonts.cpp
@@ -25,6 +25,8 @@ DialogFonts::DialogFonts(DocumentWidget *document, QWidget *parent, Qt::WindowFl
 		ui.buttonBox->removeButton(ui.buttonBox->button(QDialogButtonBox::Apply));
 		ui.buttonBox->removeButton(ui.buttonBox->button(QDialogButtonBox::Close));
 		ui.buttonBox->addButton(QDialogButtonBox::Cancel);
+		ui.fontSetAsDef->setChecked(true);
+		ui.fontSetAsDef->setEnabled(false);
 	}
 
 	const QFont font = document ? Font::fromString(document->fontName_) : Font::fromString(Preferences::GetPrefFontName());
@@ -76,9 +78,18 @@ void DialogFonts::updateFont() {
 	font.setPointSize(ui.fontSize->currentData().toInt());
 	QString fontName = font.toString();
 
-	if (document_) {
-		document_->action_Set_Fonts(fontName);
+	const bool applyToAll = ui.fontApplyAll->isChecked();
+	const bool setAsDef = ui.fontSetAsDef->isChecked();
+
+	if (applyToAll) {
+		for (DocumentWidget *doc : DocumentWidget::allDocuments()) {
+			doc->action_Set_Fonts(fontName);
+		}
 	} else {
+		document_->action_Set_Fonts(fontName);
+	}
+
+	if (setAsDef) {
 		Preferences::SetPrefFont(fontName);
 	}
 }
