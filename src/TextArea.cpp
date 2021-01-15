49c81b735a021a674be544a29a29b188b73bd21a
Evan Teran <evan.teran@gmail.com>
2021-01-14 23:19:45 -0500

    Display the last line, even if only partially by effectively rounding up.

--- src/TextArea.cpp
+++ src/TextArea.cpp
@@ -3241,7 +3241,7 @@ void TextArea::handleResize(bool widthChanged) {
 
 	const QRect viewRect      = viewport()->contentsRect();
 	const int oldVisibleLines = nVisibleLines_;
-	const int newVisibleLines = viewRect.height() / fixedFontHeight_;
+	const int newVisibleLines = (viewRect.height() / fixedFontHeight_) + 1;
 
 	/* In continuous wrap mode, a change in width affects the total number of
 	   lines in the buffer, and can leave the top line number incorrect, and
