959597a9c1bf2ad07aae3ec34667b0f6b2f82dca
Evan Teran <evan.teran@gmail.com>
2021-01-22 16:56:58 -0500

    Hopefully a fix for issue #270 (again). Now we only add one to the line count if a partial line would have been visible

--- src/TextArea.cpp
+++ src/TextArea.cpp
@@ -3241,7 +3241,10 @@ void TextArea::handleResize(bool widthChanged) {
 
 	const QRect viewRect      = viewport()->contentsRect();
 	const int oldVisibleLines = nVisibleLines_;
-	const int newVisibleLines = (viewRect.height() / fixedFontHeight_) + 1;
+	int newVisibleLines       = (viewRect.height() / fixedFontHeight_);
+	if ((viewRect.height() % fixedFontHeight_) != 0) {
+		++newVisibleLines;
+	}
 
 	/* In continuous wrap mode, a change in width affects the total number of
 	   lines in the buffer, and can leave the top line number incorrect, and
