90af0aec4e30f6b82e729d32d6eb78d71d26f548
Evan Teran <evan.teran@gmail.com>
2025-04-28 11:07:09 -0400

    forgot to commit this file

--- src/Highlight.h
+++ src/Highlight.h
@@ -52,21 +52,21 @@ struct ParseContext {
 
 bool FontOfNamedStyleIsBold(const QString &styleName);
 bool FontOfNamedStyleIsItalic(const QString &styleName);
-void LoadHighlightString(const QString &string);
 bool NamedStyleExists(const QString &styleName);
 bool ParseString(const HighlightData *pattern, const char *&string_ptr, uint8_t *&style_ptr, int64_t length, const ParseContext *ctx, const char *look_behind_to, const char *match_to);
 HighlightData *patternOfStyle(const std::unique_ptr<HighlightData[]> &patterns, uint8_t style);
-size_t FindTopLevelParentIndex(const std::vector<HighlightPattern> &patterns, size_t index);
-size_t IndexOfNamedPattern(const std::vector<HighlightPattern> &patterns, const QString &name);
 int GetPrevChar(TextBuffer *buf, TextCursor pos);
 PatternSet *FindPatternSet(const QString &languageMode);
 QString BgColorOfNamedStyle(const QString &styleName);
 QString FgColorOfNamedStyle(const QString &styleName);
 QString WriteHighlightString();
+size_t FindTopLevelParentIndex(const std::vector<HighlightPattern> &patterns, size_t index);
+size_t IndexOfNamedPattern(const std::vector<HighlightPattern> &patterns, const QString &name);
 size_t IndexOfNamedStyle(const QString &styleName);
 std::optional<PatternSet> ReadDefaultPatternSet(const QString &langModeName);
 TextCursor BackwardOneContext(TextBuffer *buf, const ReparseContext &context, TextCursor fromPos);
 TextCursor ForwardOneContext(TextBuffer *buf, const ReparseContext &context, TextCursor fromPos);
+void LoadHighlightString(const QString &string);
 void RenameHighlightPattern(const QString &oldName, const QString &newName);
 void SyntaxHighlightModifyCallback(TextCursor pos, int64_t nInserted, int64_t nDeleted, int64_t nRestyled, std::string_view deletedText, void *user);
 
