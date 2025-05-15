59c4db0fcda818ef85cda3d6f91338d5d68fb8ea
Evan Teran <evan.teran@gmail.com>
2025-05-15 12:25:22 -0400

    minor refactoring to cleanup

--- src/TextBufferFwd.h
+++ src/TextBufferFwd.h
@@ -10,10 +10,10 @@ class BasicTextBuffer;
 
 using TextBuffer = BasicTextBuffer<char>;
 
-// TODO(eteran): it seems that technically, std::char_traits<uint8_t> is not
-// supported by the C++ standard, but it works in practice. So we had to
-// implement our own traits class for uint8_t to be standard
-// compliant. (Adapted from LLVM's libc++ std::char_traits<Ch>)
+// TODO(eteran): it seems that std::char_traits<uint8_t> is actually not
+// supported by the C++ standard even though it has worked in practice.
+// So we have to implement our own traits class for uint8_t to be standard
+// compliant.
 namespace std {
 template <>
 struct char_traits<uint8_t> {
@@ -23,52 +23,43 @@ struct char_traits<uint8_t> {
 	using pos_type   = std::streampos;
 	using state_type = mbstate_t;
 
-	static void constexpr assign(char_type &c1, const char_type &c2) noexcept { c1 = c2; }
-	static constexpr bool eq(char_type c1, char_type c2) noexcept { return c1 == c2; }
-	static constexpr bool lt(char_type c1, char_type c2) noexcept { return c1 < c2; }
-	static constexpr int_type not_eof(int_type ch) noexcept { return eq_int_type(ch, eof()) ? ~eof() : ch; }
-	static constexpr char_type to_char_type(int_type ch) noexcept { return char_type(ch); }
-	static constexpr int_type to_int_type(char_type ch) noexcept { return int_type(ch); }
-	static constexpr bool eq_int_type(int_type c1, int_type c2) noexcept { return c1 == c2; }
-	static constexpr int_type eof() noexcept { return int_type(EOF); }
+	constexpr static void assign(char_type &c1, const char_type &c2) noexcept { c1 = c2; }
+	constexpr static bool eq(char_type c1, char_type c2) noexcept { return c1 == c2; }
+	constexpr static bool lt(char_type c1, char_type c2) noexcept { return c1 < c2; }
+	constexpr static int_type not_eof(int_type ch) noexcept { return eq_int_type(ch, eof()) ? ~eof() : ch; }
+	constexpr static char_type to_char_type(int_type ch) noexcept { return char_type(ch); }
+	constexpr static int_type to_int_type(char_type ch) noexcept { return int_type(ch); }
+	constexpr static bool eq_int_type(int_type c1, int_type c2) noexcept { return c1 == c2; }
+	constexpr static int_type eof() noexcept { return int_type(EOF); }
 
-	static constexpr int compare(const char_type *s1, const char_type *s2, size_t n) {
-		int ret           = 0;
-		const uint8_t *p1 = s1;
-		const uint8_t *p2 = s2;
-
-		while (!ret && n--) {
-			ret = static_cast<int>(*p1++ - *p2++);
-		}
-
-		return ret;
-	}
-
-	static constexpr size_t length(const char_type *s) {
-		const uint8_t *s_ptr = s;
-
-		while (*s != uint8_t(0)) {
-			++s;
-		}
-
-		return static_cast<size_t>(s - s_ptr);
-	}
-
-	static constexpr const char_type *find(const char_type *s, size_t n, const char_type &ch) {
-		const uint8_t *s_ptr = s;
-
-		while (n--) {
-			if (*s_ptr == ch) {
-				return s_ptr;
+	constexpr static int compare(const char_type *s1, const char_type *s2, size_t n) noexcept {
+		for (size_t i = 0; i < n; ++i) {
+			if (s1[i] != s2[i]) {
+				return s1[i] < s2[i] ? -1 : 1;
 			}
-			++s_ptr;
 		}
+		return 0;
+	}
 
+	constexpr static size_t length(const char_type *s) noexcept {
+		size_t n = 0;
+		while (*s++ != char_type(0)) {
+			++n;
+		}
+		return n;
+	}
+
+	constexpr static const char_type *find(const char_type *s, size_t n, const char_type &ch) noexcept {
+		for (size_t i = 0; i < n; ++i) {
+			if (s[i] == ch) {
+				return &s[i];
+			}
+		}
 		return nullptr;
 	}
 
-	static char_type *move(char_type *s1, const char_type *s2, size_t n) {
-		if (n == 0) {
+	constexpr static char_type *move(char_type *s1, const char_type *s2, size_t n) noexcept {
+		if (n == 0 || s1 == s2) {
 			return s1;
 		}
 
@@ -76,33 +67,23 @@ struct char_traits<uint8_t> {
 			return copy(s1, s2, n);
 		}
 
-		char_type *r = s1;
-
-		if (s2 < s1) {
-			s1 += n;
-			s2 += n;
-			for (; n; --n) {
-				assign(*--s1, *--s2);
-			}
-		}
-		return r;
-	}
-
-	static char_type *copy(char_type *s1, const char_type *s2, size_t n) {
-		char_type *r = s1;
-		for (; n; --n, ++s1, ++s2) {
-			assign(*s1, *s2);
-		}
-		return r;
-	}
-
-	static char_type *assign(char_type *s, size_t n, char_type ch) {
-		uint8_t *ptr = s;
-
 		while (n--) {
-			*ptr++ = ch;
+			assign(s1[n], s2[n]);
 		}
+		return s1;
+	}
 
+	constexpr static char_type *copy(char_type *s1, const char_type *s2, size_t n) noexcept {
+		for (size_t i = 0; i < n; ++i) {
+			assign(s1[i], s2[i]);
+		}
+		return s1;
+	}
+
+	constexpr static char_type *assign(char_type *s, size_t n, char_type ch) noexcept {
+		for (size_t i = 0; i < n; ++i) {
+			assign(s[i], ch);
+		}
 		return s;
 	}
 };
